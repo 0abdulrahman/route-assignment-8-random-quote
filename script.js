5444b8e11b160807a08a383fecf917289270c34b
7saru <saboo9653@gmail.com>
2023-07-18 00:01:15 +0300

    fixing random number generator

--- script.js
+++ script.js
@@ -5,21 +5,17 @@ const hadithGenerateBtn = document.getElementById("generator");
 async function getHadith() {
   const data = await fetch("./hadith.json");
   const jsonData = await data.json();
-  let hadithNumber = 0;
 
-  (function () {
-    let newHadithNumber = Math.floor(Math.random() * jsonData.length);
-    if (hadithNumber !== newHadithNumber) {
-      hadithNumber = newHadithNumber;
-    } else {
-      hadithNumber = Math.floor(Math.random() * jsonData.length);
-    }
-  })();
+  let hadithNumber = Math.floor(Math.random() * jsonData.length);
+  let newHadithNumber = Math.floor(Math.random() * jsonData.length);
+  while (hadithNumber === newHadithNumber) {
+    newHadithNumber = Math.floor(Math.random() * jsonData.length);
+  }
+  hadithNumber = newHadithNumber;
 
   (function () {
     const hadith = jsonData[hadithNumber].body;
     const hadithNarattor = jsonData[hadithNumber].narrator;
-
     haidthParagraph.innerText = hadith;
     hadithNarattorspan.innerText = hadithNarattor;
   })();
